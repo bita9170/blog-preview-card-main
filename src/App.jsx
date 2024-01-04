bd1af9e9985c30133eb024dc6a8ac7029854c99b
bita9170 <bita.s9170@gmail.com>
2024-01-04 14:46:14 +0100

    [FM-3] edited Font & scss upgrade

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import "./main.scss";
-import imageAvatar from "./assets/images/image-avatar.webp";
 import cardHeader from "./assets/images/illustration-article.svg";
+import imageAvatar from "./assets/images/image-avatar.webp"
+
 
 function App() {
   return (
     <main>
       <div>
         <img src={cardHeader} alt="blog card" />
-        <br />
-        <button>Learning</button> <br />
-        <br />
-        <p>published 21 Dec 2023</p> <br />
-        <h2>HTML & CSS foundations</h2>
+        <button>Learning</button>
+        <p>published 21 Dec 2023</p>
+        <h1>HTML & CSS foundations</h1>
         <p>
           These languages are the backbone of every website, defining structure,
           content, and presentation.{" "}
         </p>
+        
+        <footer>
         <img src={imageAvatar} alt="blog card" />
         <p>Greg Hooper</p>
+        </footer>
       </div>
     </main>
   );
