c1688ccc5a389a8f27edc374db8f6b92bb4845ff
bita9170 <bita.s9170@gmail.com>
2024-01-04 12:57:50 +0100

    [FM-3] edited Font & added header img

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
-// import imageAvatar from "./src/assets/images/image-avatar.jpg";
+import React from "react";
+import "./main.scss";
+import imageAvatar from "./assets/images/image-avatar.webp";
+import cardHeader from "./assets/images/illustration-article.svg";
 
 function App() {
   return (
     <main>
       <div>
-        <img src="" alt="blog card" /><br />
+        <img src={cardHeader} alt="blog card" />
+        <br />
         <button>Learning</button> <br />
         <br />
-        <p>
-          published 21 Dec 2023
-        </p> <br />
+        <p>published 21 Dec 2023</p> <br />
         <h2>HTML & CSS foundations</h2>
-        <p>These languages are the backbone of every website, defining structure, content, and presentation. </p>
-        <img src="./src/assets/images/image-avatar.webp" alt="blog card" />
-        <h4>Greg Hooper</h4>
+        <p>
+          These languages are the backbone of every website, defining structure,
+          content, and presentation.{" "}
+        </p>
+        <img src={imageAvatar} alt="blog card" />
+        <p>Greg Hooper</p>
       </div>
     </main>
   );
