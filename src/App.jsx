6f6dd04e3af13dda60d74364934fdbf8281753db
bita9170 <bita.s9170@gmail.com>
2024-01-04 01:24:38 +0100

    edited packege-lock.json & vite.config  added scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
+import React from 'react';
+// import imageAvatar from "./src/assets/images/image-avatar.jpg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <main>
-      <div><h1>Hello</h1></div>
+      <div>
+        <img src="" alt="blog card" /><br />
+        <button>Learning</button> <br />
+        <br />
+        <p>
+          published 21 Dec 2023
+        </p> <br />
+        <h2>HTML & CSS foundations</h2>
+        <p>These languages are the backbone of every website, defining structure, content, and presentation. </p>
+        <img src="./src/assets/images/image-avatar.webp" alt="blog card" />
+        <h4>Greg Hooper</h4>
+      </div>
     </main>
-  )
+  );
 }
-
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'; 
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  alias: {
+    'react/jsx-dev-runtime': 'preact/compat/jsx-dev-runtime',
+    'react/jsx-runtime': 'preact/compat/jsx-runtime',
+  },
+
   base: "/Blog-preview-card-main/",
   plugins: [react()],
 });
